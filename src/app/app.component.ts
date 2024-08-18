b56a80db7f271580f5e14d9e9ea0a8f0fc9e6223
TheoCaesar <theocaesar7@gmail.com>
2024-08-18 23:54:01 +0100

    services and standalone root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AnalyticsService } from './shared/analytics.service';
 
 @Component({
   standalone: true,
@@ -7,5 +8,6 @@ import { WelcomeComponent } from './welcome/welcome.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // providers:[AnalyticsService] - not recommended 4 DI
 })
 export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import {bootstrapApplication} from "@angular/platform-browser";
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
 // platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers:[AnalyticsService]
+});
