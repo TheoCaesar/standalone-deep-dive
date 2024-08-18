a13f62e128c1bd2a998bc1d00bb31f61e646a3bf
TheoCaesar <theocaesar7@gmail.com>
2024-08-19 00:49:02 +0100

    start point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
+
 import { WelcomeComponent } from './welcome/welcome.component';
-import { AnalyticsService } from './shared/analytics.service';
 
 @Component({
   standalone: true,
-  imports:[WelcomeComponent],
+  imports: [WelcomeComponent,],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // providers:[AnalyticsService] - not recommended 4 DI
 })
 export class AppComponent {}
