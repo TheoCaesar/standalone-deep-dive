56ee1d20eab685cc82c3b2e02eabc31e59680e7d
TheoCaesar <theocaesar7@gmail.com>
2024-08-19 01:44:08 +0100

    importProvidersFrom - app routing app wide

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import {bootstrapApplication} from "@angular/platform-browser";
-// import { AppModule } from './app/app.module';
 import { AppComponent } from './app/app.component';
-
 import { environment } from './environments/environment';
 import { AnalyticsService } from './app/shared/analytics.service';
+import { AppRoutingModule } from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-// platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
 bootstrapApplication(AppComponent, {
-  providers:[AnalyticsService]
+  providers:[
+    AnalyticsService,
+    importProvidersFrom(AppRoutingModule),
+  ]
 });
